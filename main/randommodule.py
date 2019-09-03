5d723894af46744537f0f41c8ed8a69ca6531266
umhaneul <asd7g123@naver.com>
2019-09-03 11:23:23 +0900

    add randommodule

---
+++ main/randommodule.py
@@ -0,0 +1,31 @@
+# **
+import random
+
+print("0 이상 1미만 실수 값")
+print("random.random() : ",random.random())
+print()
+print("시작값 2.5 이상 끝값 10.0 미만 실수 값")
+print("random.uniform(2.5, 10.0) : ", random.uniform(2.5, 10.0))
+print()
+print("0 이상 끝값 10미만 정수 값")
+print("random.random() : ",random.randrange(10))
+print()
+print("1 이상 끝값 7미만, 증가 값이 2인 정수 값")
+print("random.randrange(1,7,2) : ",random.randrange(1, 7, 2))
+print()
+print("리스트에서 1개 값 꺼내오기")
+season = ["봄","여름","가을","겨울"]
+print("season = ", season)
+print("random.choice(season) : ", random.choice(season))
+print()
+l = ["가","나","다","라","마"]
+print("리스트 순서를 섞기")
+random.shuffle(l)
+print("섞기 전 ㅣ 섞기")
+print("random.shuffle(ㅣ)")
+print("섞은 후 ㅣ = ",l)
+print()
+print("리스트를 몇 개의 값을 중복하지 않고 3개 뽑기")
+sample = ["1번","2번","3번","4번","5번","6번","7번","8번","9번"]
+print("샘플대상 = ",sample)
+print("random.sample(sample,3) : ",random.sample(sample,3))
