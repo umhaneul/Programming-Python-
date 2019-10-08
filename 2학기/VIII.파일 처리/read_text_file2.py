a6bb6dce495540c5b754b4d92fdd3605da653e55
umhaneul <asd7g123@naver.com>
2019-10-08 11:07:44 +0900

    write text(ama) file, read text(ama) file, readline, 가격의 총합

--- 2학기/VIII.파일 처리/read_text_file2.py
+++ 2학기/VIII.파일 처리/read_text_file2.py
@@ -1,11 +1,15 @@
 fi = open("history.ama","r",encoding="utf8")
+sum = 0
 while True :
     data = fi.readline()
     if not data :
         break
     print(data, end="")
     # 한 줄 안에서 이름 가격, ... 자르기
+    l = data.split("\t") # \t, \n, 띄어쓰기 있는걸 다 자름
+    sum += int(l[1]) #음료의 가격 합계를 구한다.
     # 총금액 계산하고, 출력하기
+print("총 금액: " + str(sum) + "원")
 fi.close()
 
 #print(data)
--- 2학기/VIII.파일 처리/wrtite_text_file4.py
+++ 2학기/VIII.파일 처리/wrtite_text_file4.py
@@ -1,7 +1,7 @@
 fi = open("history.ama","w",encoding="utf8")
 
-fi.write("아메리카노\t2300원\t레귤러\t50%\t100%\t코코\n")
-fi.write("타로버블티\t3200원\t점보\t100%\t100%\t타피오카\n")
-fi.write("티라미수버블티\t3800원\t점보\t0%\t100%\t타피오카\n")
+fi.write("아메리카노\t2300\t\t0\t50%\t2%\t코코\n")
+fi.write("타로버블티\t3200\t\t1\t100%\t2%\t타피오카\n")
+fi.write("티라미수버블티\t3800\t1\t0%\t0%\t타피오카\n")
 
 fi.close()
