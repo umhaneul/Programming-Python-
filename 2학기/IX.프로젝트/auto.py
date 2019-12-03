7a2722162b7fe0b74bfe5f5e2877f96f41efac44
umhaneul <asd7g123@naver.com>
2019-12-03 12:05:43 +0900

    PyAutoGUI - 마우스 이동, 클릭, 키보드 타이핑, 특수키

---
+++ 2학기/IX.프로젝트/auto.py
@@ -0,0 +1,26 @@
+#PyAutoGUI 설치하자
+#pip install pyautogui
+import time
+import pyautogui as pag
+
+if __name__ == '__main__' :
+    # while True:
+    #     x, y = pag.position()
+    #     print("x : {} \ty: {}".format(x,y), end="\r")   #r=내리지 않고 맨 앞으로
+    # pag.moveTo(230,374, duration=2)  #~어디 이동
+    # pag.move(100,200, duration=2)     #~만큼 이동
+    # pag.click()
+    # pag.click()
+    # pag.doubleClick() #더블클릭
+    # pag.click(clicks=2)
+    # pag.drag(0, 200, duration=1)  #드래그 duration있어야 함
+    # pag.rightClick()  #오른쪽 버튼
+    # pag.click(230,374, duration=2)
+    pag.doubleClick(230,374,duration=2)
+    # TODO: pag.scroll(0, 100)
+    time.sleep(1)   #크롬이 열리기를 기다려야 함
+    pag.typewrite("http://ticket.interpark.com/")
+    pag.press("enter")
+    # pag.press("hangul")   #한/영키
+    # pag.typewrite("아이유")  #아이유 영어로
+    pag.press("enter")
