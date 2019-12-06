c14cadbbf1615e1f529e1932ec8ae5e08f996351
umhaneul <asd7g123@naver.com>
2019-12-06 14:24:00 +0900

    PyAutoGUI - 마우스 이동, 클릭, 키보드 타이핑, 특수키, 스크롤

--- 2학기/IX.프로젝트/auto.py
+++ 2학기/IX.프로젝트/auto.py
@@ -16,11 +16,15 @@ if __name__ == '__main__' :
     # pag.drag(0, 200, duration=1)  #드래그 duration있어야 함
     # pag.rightClick()  #오른쪽 버튼
     # pag.click(230,374, duration=2)
-    pag.doubleClick(230,374,duration=2)
-    # TODO: pag.scroll(0, 100)
-    time.sleep(1)   #크롬이 열리기를 기다려야 함
-    pag.typewrite("http://ticket.interpark.com/")
-    pag.press("enter")
-    # pag.press("hangul")   #한/영키
-    # pag.typewrite("아이유")  #아이유 영어로
-    pag.press("enter")
+    #pag.doubleClick(230,374,duration=2)
+    # time.sleep(1)   #크롬이 열리기를 기다려야 함
+    # pag.typewrite("http://ticket.interpark.com/")
+    # pag.press("enter")
+    # # pag.press("hangul")   #한/영키
+    # # pag.typewrite("아이유")  #아이유 영어로
+    # pag.press("enter")
+    # TODO: pag.scroll
+    pag.hotkey("alt","tab") #단축키
+    time.sleep(2)
+    # pag.scroll(clicks=-2000, x=1018, y=304)
+    pag.scroll(-2000, x=1018, y=304)
