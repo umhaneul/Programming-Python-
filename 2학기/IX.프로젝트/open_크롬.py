f1b16cc533acb7d62042b608df6fd771777bb278
umhaneul <asd7g123@naver.com>
2019-12-06 14:40:13 +0900

    PyAutoGUI - locateOnScrean()

---
+++ 2학기/IX.프로젝트/open_크롬.py
@@ -0,0 +1,11 @@
+import pyautogui as pag
+import time
+
+if __name__ == '__main__' :
+    data = pag.locateOnScreen("크롬.PNG")
+    print(data)
+    #pag.moveTo(data.left+(data.width/2), data.top+(data.heigth/2), duration=2)
+    center = pag.center(data)
+    print(center)
+    pag.moveTo(center, duration=2)
+    pag.doubleClick()
